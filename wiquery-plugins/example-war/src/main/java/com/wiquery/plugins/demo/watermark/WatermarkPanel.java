c877befc5a01d714df59f93a3dcc09b4320bde3a
reiern70 <reiern70@e9f5dc0e-4f7d-fa4d-8f3f-b1228421604b>
2010-05-24 06:41:43 +0000

    implementing watermark

--- wiquery-plugins/example-war/src/main/java/com/wiquery/plugins/demo/watermark/WatermarkPanel.java
+++ wiquery-plugins/example-war/src/main/java/com/wiquery/plugins/demo/watermark/WatermarkPanel.java
@@ -4,7 +4,7 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 
-import com.wiquery.pluglin.watermark.TextFieldWatermarkBehaviour;
+import com.wiquery.plugin.watermark.TextFieldWatermarkBehaviour;
 
 /**
  * 
