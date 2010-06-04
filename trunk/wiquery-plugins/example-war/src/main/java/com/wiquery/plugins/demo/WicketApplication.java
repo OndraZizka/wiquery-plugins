86ad9fbaeda59afe4e77ac4468af17cb6002281d
reiern70 <reiern70@e9f5dc0e-4f7d-fa4d-8f3f-b1228421604b>
2010-06-04 20:26:15 +0000

    disabled drag-drop for IE

--- trunk/wiquery-plugins/example-war/src/main/java/com/wiquery/plugins/demo/WicketApplication.java
+++ trunk/wiquery-plugins/example-war/src/main/java/com/wiquery/plugins/demo/WicketApplication.java
@@ -56,9 +56,9 @@ public class WicketApplication extends WiQueryWebApplication implements IThemabl
     			Person person = new Person();
     			person.setId(new Long(i));
     			person.setName("Name-" + i);
-    			//person.setLastName("LastName-" + i);
+    			person.setLastName("LastName-" + i);
     			persons.add(person);
-    		}
+    		}    		
     	}
     	return persons;
     }
