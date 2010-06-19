51ce96bff6bf1f89512821a28e8dae62e2213f92
reiern70 <reiern70@e9f5dc0e-4f7d-fa4d-8f3f-b1228421604b>
2010-06-19 15:51:48 +0000

    jQueryUI based slide deck

--- trunk/wiquery-plugins/example-war/src/main/java/com/wiquery/plugins/demo/SlideDeckPanel.java
+++ trunk/wiquery-plugins/example-war/src/main/java/com/wiquery/plugins/demo/SlideDeckPanel.java
@@ -3,7 +3,16 @@
  */
 package com.wiquery.plugins.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.wiquery.plugins.slidedeck.IDeck;
+import org.wiquery.plugins.slidedeck.JQUISlideDeck;
 import org.wiquery.plugins.slidedeck.SlideDeck;
 
 /**
@@ -20,7 +29,44 @@ public class SlideDeckPanel extends Panel {
 	public SlideDeckPanel(String id) {
 		super(id);
 		
-		SlideDeck slidedeck = new SlideDeck("slidedeck");
+		List<IDeck> decks = new ArrayList<IDeck>();
+		decks.add(new IDeck() {
+			
+			private static final long serialVersionUID = 1L;
+
+			public IModel<String> getDeckTitle() {
+				return new Model<String>("Slide 1");
+			}
+			
+			public Component getDeckContents(String id) {
+				return new Label(id, "Slide 1");
+			}
+		});
+		decks.add(new IDeck() {
+			
+			private static final long serialVersionUID = 1L;
+
+			public IModel<String> getDeckTitle() {
+				return new Model<String>("Slide 2");
+			}
+			
+			public Component getDeckContents(String id) {
+				return new Label(id, "Slide 3");
+			}
+		});
+		decks.add(new IDeck() {
+			
+			private static final long serialVersionUID = 1L;
+
+			public IModel<String> getDeckTitle() {
+				return new Model<String>("Slide 3");
+			}
+			
+			public Component getDeckContents(String id) {
+				return new Label(id, "Slide 3");
+			}
+		});
+		JQUISlideDeck slidedeck = new JQUISlideDeck("slidedeck", decks);
 		add(slidedeck);
 		
 	}
