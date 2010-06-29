5a25bb5b53b47faf7b70a47d3049802633531e75
reiern70 <reiern70@e9f5dc0e-4f7d-fa4d-8f3f-b1228421604b>
2010-06-29 20:43:49 +0000

    Share project "wiquery-plugins-demo" into "https://wiquery-plugins.googlecode.com/svn"

---
+++ wiquery-plugins-demo/src/com/inqle/web/BasePage.java
@@ -0,0 +1,23 @@
+package com.inqle.web;
+
+import org.apache.wicket.markup.html.WebPage;
+
+
+/**
+ * Base page.
+ */
+public class BasePage extends WebPage {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructor that is invoked when page is invoked without a session.
+	 * 
+	 * @param parameters
+	 *            Page parameters
+	 */
+    public BasePage() {
+
+    }
+    
+}
