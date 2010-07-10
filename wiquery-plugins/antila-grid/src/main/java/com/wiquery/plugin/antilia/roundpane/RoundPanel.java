8005e3c7e00d40dfe26c5e2fbfea036ffa395c50
reiern70 <reiern70@e9f5dc0e-4f7d-fa4d-8f3f-b1228421604b>
2010-07-10 06:39:29 +0000

    round corners component

--- wiquery-plugins/antila-grid/src/main/java/com/wiquery/plugin/antilia/roundpane/RoundPanel.java
+++ wiquery-plugins/antila-grid/src/main/java/com/wiquery/plugin/antilia/roundpane/RoundPanel.java
@@ -1,10 +1,15 @@
 package com.wiquery.plugin.antilia.roundpane;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.ResourceReference;
 import org.apache.wicket.markup.html.CSSPackageResource;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.EmptyPanel;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IModel;
 
 /**
  * @author Ernesto Reinaldo Barreiro
@@ -16,23 +21,51 @@ public class RoundPanel extends Panel {
 
 	private static final ResourceReference CSS = new ResourceReference(RoundPanel.class,"test.css");
 	
+	private WebMarkupContainer root;
+	
+	private Integer width = 300;
 	
 	/**
 	 * @param id
 	 */
-	public RoundPanel(String id) {
+	public RoundPanel(String id, IModel<String> titleModel) {
 		super(id);
+		setRenderBodyOnly(true);
 		add(CSSPackageResource.getHeaderContribution(CSS));
+		root = new WebMarkupContainer("root") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void onBeforeRender() {
+				if(get("content")== null) {
+					add(RoundPanel.this.newContent("content"));
+				}
+				super.onBeforeRender();
+			}
+		};
+		root.setOutputMarkupId(true);
+		root.add(new AttributeModifier("style", new AbstractReadOnlyModel<String>() {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public String getObject() {				
+				StringBuffer sb = new StringBuffer();
+				if(width != null) {
+					sb.append("width: ")
+					.append(width)
+					.append("px");					
+				}
+				return sb.toString();
+			}
+		}));
+		add(root);
+		
+		root.add(new Label("title", titleModel));
+		
 	}
-	
-	@Override
-	protected void onBeforeRender() {
-		if(get("content")== null) {
-			add(newContent("content"));
-		}
-		super.onBeforeRender();
-	}
-	
+			
 	protected Component newContent(String id) {
 		return new EmptyPanel(id);
 	}
