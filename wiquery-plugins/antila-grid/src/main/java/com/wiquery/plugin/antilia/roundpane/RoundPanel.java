693fe2d122d83b3c4cc7590c8bf14cfe0c3f0f9a
reiern70 <reiern70@e9f5dc0e-4f7d-fa4d-8f3f-b1228421604b>
2010-07-11 14:31:43 +0000

    round corners component

--- wiquery-plugins/antila-grid/src/main/java/com/wiquery/plugin/antilia/roundpane/RoundPanel.java
+++ wiquery-plugins/antila-grid/src/main/java/com/wiquery/plugin/antilia/roundpane/RoundPanel.java
@@ -3,6 +3,7 @@ package com.wiquery.plugin.antilia.roundpane;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.ResourceReference;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.CSSPackageResource;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
@@ -10,6 +11,11 @@ import org.apache.wicket.markup.html.panel.EmptyPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
+
+import com.wiquery.plugins.antilia.link.JQIcon;
+import com.wiquery.plugins.antilia.link.JqAjaxLink;
+import com.wiquery.plugins.antilia.menu.IMenu;
+import com.wiquery.plugins.antilia.menu.Menu;
 
 /**
  * @author Ernesto Reinaldo Barreiro
@@ -25,6 +31,24 @@ public class RoundPanel extends Panel {
 	
 	private Integer width = 300;
 	
+	private Integer heigh = 400;
+	
+	public Integer getHeigh() {
+		return heigh;
+	}
+
+	public void setHeigh(Integer heigh) {
+		this.heigh = heigh;
+	}
+
+	public Integer getWidth() {
+		return width;
+	}
+
+	public void setWidth(Integer width) {
+		this.width = width;
+	}
+
 	/**
 	 * @param id
 	 */
@@ -55,15 +79,44 @@ public class RoundPanel extends Panel {
 				if(width != null) {
 					sb.append("width: ")
 					.append(width)
-					.append("px");					
+					.append("px;");					
+				}
+				if(heigh != null) {
+					sb.append("height: ")
+					.append(heigh)
+					.append("px;");
 				}
 				return sb.toString();
 			}
 		}));
 		add(root);
 		
-		root.add(new Label("title", titleModel));
+		root.add(new Label("title", titleModel));				
+		Menu menu = new Menu("menu");
+		configureMenu(menu);
+		root.add(menu);		
+	}
+	
+	protected void configureMenu(IMenu menu) {
+		menu.addMenuItem(new JqAjaxLink(menu.newItemId(), JQIcon.ui_icon_circle_close, "Close") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+					
+			}
+		});
 		
+		menu.addMenuItem(new JqAjaxLink(menu.newItemId(), JQIcon.ui_icon_circle_plus, "Close") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+					
+			}
+		});
 	}
 			
 	protected Component newContent(String id) {
