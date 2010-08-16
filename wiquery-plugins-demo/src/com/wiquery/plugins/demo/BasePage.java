b415d5679ce2f28c13cbeee2103f832ffd4232e4
reiern70 <reiern70@e9f5dc0e-4f7d-fa4d-8f3f-b1228421604b>
2010-08-16 15:50:54 +0000

    added a resizable example

--- wiquery-plugins-demo/src/com/wiquery/plugins/demo/BasePage.java
+++ wiquery-plugins-demo/src/com/wiquery/plugins/demo/BasePage.java
@@ -4,6 +4,7 @@ import org.apache.wicket.markup.html.CSSPackageResource;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.odlabs.wiquery.plugin.layout.Layout;
+import org.odlabs.wiquery.plugin.layout.Layout.PanePositionEnum;
 
 import com.wiquery.plugins.antilia.grid.resources.DefaultStyle;
 import com.wiquery.plugins.demo.resources.Styles;
@@ -28,10 +29,14 @@ public abstract class BasePage extends WebPage {
         	
         	private static final long serialVersionUID = 1L;
 
-
         	@Override
-        	public Panel getLayoutEastComponent(String wicketId) {
+        	public Panel getLayoutWestComponent(String wicketId) {
         		return new NavigationPanel(wicketId);
+        	}
+        	
+        	@Override
+        	public Panel getLayoutNorthComponent(String wicketId) {
+        		return new HeaderPanel(wicketId);
         	}
         	
         	@Override
@@ -41,6 +46,12 @@ public abstract class BasePage extends WebPage {
         	
         };
         add(layout);
+        layout.setResizable(PanePositionEnum.NORTH, false)
+        .setClosable(PanePositionEnum.NORTH, false)
+        .setSpacingOpen(PanePositionEnum.NORTH, 0)
+        .setMinSize(PanePositionEnum.NORTH, 80)
+        .setMaxSize(PanePositionEnum.NORTH, 102);
+        
     }
     
     protected abstract Panel getLayoutCenterComponent(String wicketId);
