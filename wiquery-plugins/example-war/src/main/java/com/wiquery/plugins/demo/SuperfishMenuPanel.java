8a968ae5e0919bc05650b979f24e7e614848d469
reiern70 <reiern70@e9f5dc0e-4f7d-fa4d-8f3f-b1228421604b>
2010-09-20 15:31:56 +0000

    adding initial implementation of superfish menu

--- wiquery-plugins/example-war/src/main/java/com/wiquery/plugins/demo/SuperfishMenuPanel.java
+++ wiquery-plugins/example-war/src/main/java/com/wiquery/plugins/demo/SuperfishMenuPanel.java
@@ -3,9 +3,17 @@
  */
 package com.wiquery.plugins.demo;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 
+import com.wiquery.plugin.superfish.AjaxMenuItem;
+import com.wiquery.plugin.superfish.BookMarkablePageMenuItem;
 import com.wiquery.plugin.superfish.Menu;
+import com.wiquery.plugin.superfish.SubMenu;
+import com.wiquery.plugin.superfish.Menu.Speed;
 
 /**
  * @author Ernesto Reinaldo Barreiro (reiern70@gmail.com)
@@ -15,15 +23,203 @@ public class SuperfishMenuPanel extends Panel {
 
 	private static final long serialVersionUID = 1L;
 	
+	private WebMarkupContainer context; 
+	
+	private String message = "";
+	
 	/**
 	 * @param id
 	 */
 	public SuperfishMenuPanel(String id) {
 		super(id);
-		add(new Menu("menu")
-		.setDropShadows(false)
-		.setAutoArrows(false)
-		);
+		
+		context = new WebMarkupContainer("context");
+		context.setOutputMarkupId(true);
+		add(context);
+		
+		context.add(new Label("message", new AbstractReadOnlyModel<String>(){
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public String getObject() {
+				return getMessage();
+			}
+		}));
+		
+		Menu menu = new Menu("menu");
+		menu.setSpeed(Speed.FAST)
+		.setDelay(300);
+		context.add(menu);
+		
+		menu.addItem(new BookMarkablePageMenuItem<HomePage>("Home") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected Class<HomePage> getPageClass() {
+				return HomePage.class;
+			}
+		});
+
+		SubMenu subMenu = new SubMenu("Sub Menu 1");
+		menu.addItem(subMenu);
+		
+		subMenu.addItem(new AjaxMenuItem("Very very very long link!") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				setMessage(getDefaultModel().getObject().toString());
+				target.addComponent(SuperfishMenuPanel.this.context);
+			}
+		});
+		subMenu.addItem(new AjaxMenuItem("Link 1.1") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				setMessage(getDefaultModel().getObject().toString());
+				target.addComponent(SuperfishMenuPanel.this.context);
+			}
+		});
+		subMenu.addItem(new AjaxMenuItem("Link 1.2") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				setMessage(getDefaultModel().getObject().toString());
+				target.addComponent(SuperfishMenuPanel.this.context);
+			}
+		});
+		
+		subMenu = new SubMenu("Sub Menu 2");
+		menu.addItem(subMenu);
+		
+		
+		SubMenu susSubMenu = new SubMenu("Sub Menu 2.1");
+		subMenu.addItem(susSubMenu);
+		
+		susSubMenu.addItem(new AjaxMenuItem("link 2.1.1") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				setMessage(getDefaultModel().getObject().toString());
+				target.addComponent(SuperfishMenuPanel.this.context);
+			}
+		});
+		susSubMenu.addItem(new AjaxMenuItem("link 2.1.2") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				setMessage(getDefaultModel().getObject().toString());
+				target.addComponent(SuperfishMenuPanel.this.context);
+			}
+		});
+		
+		susSubMenu.addItem(new AjaxMenuItem("link 2.1.3") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				setMessage(getDefaultModel().getObject().toString());
+				target.addComponent(SuperfishMenuPanel.this.context);
+			}
+		});
+		
+		susSubMenu.addItem(new AjaxMenuItem("link 2.1.4") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				setMessage(getDefaultModel().getObject().toString());
+				target.addComponent(SuperfishMenuPanel.this.context);
+			}
+		});
+		
+		SubMenu subSubSubMenu  = new SubMenu("Sub menu 2.1.5");
+		susSubMenu.addItem(subSubSubMenu);
+		
+		subSubSubMenu.addItem(new AjaxMenuItem("link 2.1.5.1") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				setMessage(getDefaultModel().getObject().toString());
+				target.addComponent(SuperfishMenuPanel.this.context);
+			}
+		});
+		
+		subSubSubMenu.addItem(new AjaxMenuItem("link 2.1.5.2") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				setMessage(getDefaultModel().getObject().toString());
+				target.addComponent(SuperfishMenuPanel.this.context);
+			}
+		});
+		
+		subMenu.addItem(new AjaxMenuItem("Link 2.2") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				setMessage(getDefaultModel().getObject().toString());
+				target.addComponent(SuperfishMenuPanel.this.context);
+			}
+		});
+		subMenu.addItem(new AjaxMenuItem("Link 2.3") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				setMessage(getDefaultModel().getObject().toString());
+				target.addComponent(SuperfishMenuPanel.this.context);
+			}
+		});
+		
+		subMenu.addItem(new AjaxMenuItem("Link 2.4") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				setMessage(getDefaultModel().getObject().toString());
+				target.addComponent(SuperfishMenuPanel.this.context);
+			}
+		});
+
+		menu.addItem(new AjaxMenuItem("Link 3") {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				setMessage(getDefaultModel().getObject().toString());
+				target.addComponent(SuperfishMenuPanel.this.context);
+			}
+		});
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
 	}
 
 }
--- wiquery-plugins/other-plugins/src/main/java/com/wiquery/plugin/superfish/Menu.java
+++ wiquery-plugins/other-plugins/src/main/java/com/wiquery/plugin/superfish/Menu.java
@@ -3,9 +3,11 @@ package com.wiquery.plugin.superfish;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.ResourceReference;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.RepeatingView;
 import org.odlabs.wiquery.core.commons.CoreJavaScriptResourceReference;
 import org.odlabs.wiquery.core.commons.IWiQueryPlugin;
 import org.odlabs.wiquery.core.commons.WiQueryResourceManager;
@@ -39,18 +41,31 @@ public class Menu extends Panel implements IMenu, IWiQueryPlugin {
 	protected static final ResourceReference VERTICAL_CSS = new ResourceReference(Menu.class, "css/superfish-vertical.css");
 	
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	private WebMarkupContainer menu;
 	
-	private Options options;
-	
-	//private RepeatingView itemsView;
+	public static enum Speed {
+		SLOW, NORMAL, FAST;
+		
+		/**
+		 * @return a non null String containing the effect speed value.
+		 */
+		public String getJavaScriptStatement() {
+			return this.name().toLowerCase();
+		}
+	}
 	
 	/**
+	 * The options.
+	 */
+	private Options options;
+	
+	private RepeatingView itemsView;
+	
+	/**
+	 * Constructor.
+	 * 
 	 * @param id
 	 */
 	public Menu(String id) {
@@ -66,13 +81,39 @@ public class Menu extends Panel implements IMenu, IWiQueryPlugin {
 		add(menu);		
 	}
 	
+	/*
+	 * (non-Javadoc)
+	 * @see org.odlabs.wiquery.core.commons.IWiQueryPlugin#contribute(org.odlabs.wiquery.core.commons.WiQueryResourceManager)
+	 */
 	public void contribute(WiQueryResourceManager wiQueryResourceManager) {
 		wiQueryResourceManager.addJavaScriptResource(CoreJavaScriptResourceReference.get());
-		wiQueryResourceManager.addJavaScriptResource(HoverIntentJavaScriptResourceReference.get());
+		// if HoverIntent scanning is disable then we do not include the JavaScript either. 		
+		if(!isDisableHI())
+			wiQueryResourceManager.addJavaScriptResource(HoverIntentJavaScriptResourceReference.get());
 		wiQueryResourceManager.addJavaScriptResource(SuperfishJavaScriptResourceReference.get());
 		wiQueryResourceManager.addCssResource(newMenuCss());
 	}
 	
+	@Override
+	protected void onBeforeRender() {
+		if(itemsView == null) {
+			itemsView = new RepeatingView("items");
+			menu.add(itemsView);
+			for(IMenuItem item: items) {
+				if(item instanceof Component) {
+					WebMarkupContainer child = new WebMarkupContainer(itemsView.newChildId());
+					itemsView.add(child);
+					child.add((Component)item);
+				}				
+			}
+		}
+		super.onBeforeRender();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.odlabs.wiquery.core.commons.IWiQueryPlugin#statement()
+	 */
 	public JsStatement statement() {
 		return new JsQuery(menu).$().chain("superfish", options.getJavaScriptOptions());
 	}
@@ -81,11 +122,17 @@ public class Menu extends Panel implements IMenu, IWiQueryPlugin {
 	 *  Flag to enable/disable drop shadows 
 	 * 
 	 * @param dropShadows
-	 *            true to drop shadow, false otherwise
+	 *            true to drop shadow, false otherwise. Use null 
+	 *            to un-set it and use default value.
 	 * @return instance of the current component
 	 */
-	public Menu setDropShadows(boolean dropShadows) {
-		options.put("dropShadows", dropShadows);
+	public Menu setDropShadows(Boolean dropShadows) {
+		if(dropShadows == null) {
+			options.removeOption("dropShadows");
+		} else {
+			options.put("dropShadows", dropShadows);
+		}
+		
 		return this;
 	}
 	
@@ -94,11 +141,74 @@ public class Menu extends Panel implements IMenu, IWiQueryPlugin {
 	 *  code at expense of initialization performance  
 	 * 
 	 * @param autoArrows
-	 *            true to auto-generate arrows, false otherwise
+	 *            true to auto-generate arrows, false otherwise.
+	 *            Use null to un-set it and use default value.
 	 * @return instance of the current component
 	 */
-	public Menu setAutoArrows(boolean autoArrows) {
-		options.put("autoArrows", autoArrows);
+	public Menu setAutoArrows(Boolean autoArrows) {
+		if(autoArrows == null) {
+			options.removeOption("autoArrows");
+		} else {			
+			options.put("autoArrows", autoArrows);
+		}
+		return this;
+	}
+	
+	/**
+	 * Set to true to disable hoverIntent detection
+	 * 
+	 * @param disableHI Set to true to disable hoverIntent detection.
+	 * @return instance of the current component
+	 */
+	public Menu setDisableHI(Boolean disableHI) {
+		if(disableHI == null) {
+			options.removeOption("disableHI");
+		} else {			
+			options.put("disableHI", disableHI);
+		}
+		return this;
+	}
+	
+	/**
+	 * Sets speed of the animation. Equivalent to second parameter of jQuery’s .animate() method
+	 * @param speed
+	 * @return
+	 */
+	public Menu setSpeed(Speed speed) {
+		if(speed == null) {
+			options.removeOption("speed");
+		} else {			
+			options.putLiteral("speed", speed.getJavaScriptStatement());
+		}
+		return this;
+	}
+	
+	
+	/**
+	 * @return Gets the disableHI option.
+	 */
+	public boolean  isDisableHI() {
+		try {
+			Boolean disableHI = options.getBoolean("disableHI");
+			return (disableHI != null)?disableHI.booleanValue(): false;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	
+	/**
+	 * The delay in milliseconds that the mouse can remain outside a sub-menu without it closing.
+	 * 
+	 * @param delay The delay in milliseconds. Use null to un-set it and use default value.
+	 * @return instance of the current component.
+	 */
+	public Menu setDelay(Integer delay) {
+		if(delay == null){
+			options.removeOption("delay");
+		} else {
+			options.put("delay", delay);
+		}
 		return this;
 	}
 	
