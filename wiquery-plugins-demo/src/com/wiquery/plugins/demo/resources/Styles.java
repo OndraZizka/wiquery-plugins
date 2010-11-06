29b7d15328b9f234bb660551c0b617e23de1e614
reiern70 <reiern70@e9f5dc0e-4f7d-fa4d-8f3f-b1228421604b>
2010-11-06 11:45:53 +0000

    Creating 1.1 release tag

--- wiquery-plugins-demo/src/com/wiquery/plugins/demo/resources/Styles.java
+++
@@ -1,9 +0,0 @@
-package com.wiquery.plugins.demo.resources;
-
-import org.apache.wicket.ResourceReference;
-
-public class Styles {
-	
-	public static ResourceReference CSS_MAIN = new ResourceReference(Styles.class, "application.css");
-
-}
