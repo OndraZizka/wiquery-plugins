eb0051ed0c81feb539b8846e7b43ac1d3e3294c8
david.leangen <david.leangen@e9f5dc0e-4f7d-fa4d-8f3f-b1228421604b>
2012-07-21 03:17:47 +0000

    Updated jqrid to version 4.4.0 (issue #24)

--- trunk/wiquery-plugins/jqgrid/src/main/java/com/wiquery/plugins/jqgrid/Resources.java
+++ trunk/wiquery-plugins/jqgrid/src/main/java/com/wiquery/plugins/jqgrid/Resources.java
@@ -10,9 +10,9 @@ import org.apache.wicket.markup.html.resources.JavascriptResourceReference;
 public class Resources {
 	
 
-	public static final ResourceReference CSS_JQUERY_JQGRID = new ResourceReference(Resources.class, "themes/ui.jqgrid.css");
+	public static final ResourceReference CSS_JQUERY_JQGRID = new ResourceReference(Resources.class, "css/ui.jqgrid.css");
 	public static final JavascriptResourceReference JS_JQUERY_LOC_EN = new JavascriptResourceReference(Resources.class, "js/i18n/grid.locale-en.js");	
 	public static final JavascriptResourceReference JS_JQUERY_JQGRID = new JavascriptResourceReference(Resources.class, "js/jquery.jqGrid.min.js");	
-	public static final JavascriptResourceReference JS_JQUERY_CONTEXT_MENU = new JavascriptResourceReference(Resources.class, "js/jquery.contextmenu.js");
-	public static final ResourceReference JS_MULTISELECT = new ResourceReference(Resources.class, "js/ui.multiselect.js");
+	public static final JavascriptResourceReference JS_JQUERY_CONTEXT_MENU = new JavascriptResourceReference(Resources.class, "plugins/jquery.contextmenu.js");
+	public static final ResourceReference JS_MULTISELECT = new ResourceReference(Resources.class, "plugins/ui.multiselect.js");
 }
